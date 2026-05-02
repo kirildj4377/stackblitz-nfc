bcbf4d337c63f65059f7318f8a0cedab96819e67
kirildj4377 <kirilandme12@gmail.com>
2026-05-02 12:46:49 +0300

    Update ProductList.tsx

--- components/ProductList.tsx
+++ components/ProductList.tsx
@@ -1,7 +1,8 @@
 import { products } from '@/data/mockProducts';
 
 export const ProductList = () => {
-  return ( <> 
+  return (
+    <> 
     <section className="py-20 bg-slate-50">
       <div className="container mx-auto px-6">
         <h2 className="text-3xl font-bold mb-12 text-slate-900">Популярные решения</h2>
@@ -26,6 +27,7 @@ export const ProductList = () => {
           ))}
         </div>
       </div>
-    </section> </>
+    </section> 
+  </>
   );
 };
